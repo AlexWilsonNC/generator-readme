3e5cb03e8de343a89971a2e2b038a1b0c186ddd6
AlexWilsonNC <alexwilson0017@yahoo.com>
2021-09-08 17:48:48 -0400

    added each needed prompt and the messsage thatll be displayed in the readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# generator-readme
-An application I've created using node.js to generate a README.md file based on user input.
+# Generator
+
+## Description
+This is a descrition.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution Guidelines](#guidelines)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+nothing
+
+## Usage
+nope
+
+## Guidelines
+[Rules to Follow - "Contributor Covenant"](https://www.contributor-covenant.org/)
+
+## Tests
+
+
+## Questions
+For your convinience, the developer's Github account is provided below.
+
+[Github Profile](https://github.com/AlexWilsonNC)
+
+For further questions, you may reach out via the following email.
+
+alexwilson0017@yahoo.com
--- index.js
+++ index.js
@@ -1,9 +1,89 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 inquirer.prompt([
-    {
+    { 
+        name: 'title',
         type: 'input',
-        message: 'What is the title of your README?',
-        name: 'title'
+        message: 'What is the title of your README?'
     },
-]);
+    { 
+        name: 'description',
+        type: 'input',
+        message: 'Type out a description for this application.'
+    },
+    {   name: 'license', 
+        type: 'checkbox',
+        message: 'Choose one license for the application.',
+        choices: ['a', 'b', 'c', 'd']
+    },
+    {   name: 'installation',
+        type: 'input',  // change?
+        message: 'What is required to install your project?'
+    },
+    { 
+        name: 'instructions', 
+        type: 'input', // have options to include 1 instruction at a time, add more until selected no
+        message: 'Any special instructions to use this?'
+    },
+    {
+        name: 'tests',
+        type: 'input',
+        message: 'asd' // idk
+    },
+    {
+        name: 'username',
+        type: 'input',
+        message: 'Provide your Github username.'
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: 'Provide your email address.'
+    },
+
+]).then((answers) => {
+    const data = answers.licenses.map(license => `- ${license}`).join('\n'); // ${data}
+
+    const template = 
+
+`# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution Guidelines](#guidelines)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.instructions}
+
+## Guidelines
+[Rules to Follow - "Contributor Covenant"](https://www.contributor-covenant.org/)
+
+## Tests
+${answers.tests}
+
+## License
+${answers.license}
+
+## Questions
+For your convinience, the developer's Github account is provided below.\n
+[Github Profile](https://github.com/${answers.username})
+
+For further questions, you may reach out via the following email.\n
+${answers.email}
+`;
+
+fs.writeFile('README.md', template, (error) => {
+        if (error) console.log(error);
+        console.log('Success');
+    })
+});
